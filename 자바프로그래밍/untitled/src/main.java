ee77b15bbc6472c487076302839cb493633ea70a
oMFDOo <jinjoo021@naver.com>
2022-05-23 18:36:38 +0900

    .

--- 자바프로그래밍/untitled/src/main.java
+++ 자바프로그래밍/untitled/src/main.java
@@ -1,46 +1,36 @@
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Map<String, String> map = new HashMap<String, String>();
+        FileReader fr;
+        DataInputStream in = null;
+        DataOutputStream out = null;
 
-        map.put("lee", "word");
-        map.put("choi", "password");
-        map.put("kim", "1234");
-        map.put("park", "pass");
 
-        for(String key:map.keySet()){
-            String value = map.get(key);
-            System.out.println("Key="+key + " Value=" + value);
-            int temp = key.hashCode() + value.hashCode();
-            System.out.println("hashCode()="+key.hashCode() + " hashCode()=" + value.hashCode() + " add=" + temp);
+        try{
+            out = new DataOutputStream(new FileOutputStream("data.bin"));
+            out.writeInt(123);
+            out.writeFloat(123.456F);
+            out.close();
+
+            in = new DataInputStream(new FileInputStream("data.bin"));
+            int aint = in.readInt();
+            float afloat = in.readFloat();
+
+            System.out.println(aint);
+            System.out.println(afloat);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(in != null){
+                in.close();
+            }
+            if(out != null) {
+                out.close();
+            }
         }
-
-
-
-//
-//        String a = "100";
-//        String b = "100";
-//        if(a.equals(b))
-//        {
-//            System.out.println("Equal variables:");
-//            System.out.println(a.hashCode() + "\n" + b.hashCode());
-//        }
-//
-//        String c = "100";
-//        String d = "500";
-//
-//        if(!c.equals(d))
-//        {
-//            System.out.println("\nUn-equal variables:");
-//            System.out.println(c.hashCode() + "\n" + d.hashCode());
-//        }
-//        String Str = new String("We are learning hashCode in Java");
-//        System.out.println("Hashcode for Str :" + Str.hashCode() );
     }
 }
 

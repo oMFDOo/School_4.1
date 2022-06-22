cd01a9b9eacdb802651c53c9948e5aeda804f14f
oMFDOo <jinjoo021@naver.com>
2022-06-22 14:01:17 +0900

    .

--- 자바프로그래밍/untitled/src/main.java
+++ 자바프로그래밍/untitled/src/main.java
@@ -1,36 +1,19 @@
 import java.io.IOException;
-import java.io.*;
+
+class myThread extends Thread {
+    public void run() {
+        for(int i = 0; i < 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
 
 public class main {
     public static void main(String[] args) throws IOException {
-        FileReader fr;
-        DataInputStream in = null;
-        DataOutputStream out = null;
-
-
-        try{
-            out = new DataOutputStream(new FileOutputStream("data.bin"));
-            out.writeInt(123);
-            out.writeFloat(123.456F);
-            out.close();
-
-            in = new DataInputStream(new FileInputStream("data.bin"));
-            int aint = in.readInt();
-            float afloat = in.readFloat();
-
-            System.out.println(aint);
-            System.out.println(afloat);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(in != null){
-                in.close();
-            }
-            if(out != null) {
-                out.close();
-            }
-        }
+        Thread t1 = new myThread();
+        Thread t2 = new myThread();
+        t1.start();
+        t2.start();
     }
 }
 
